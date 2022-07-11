65967974134500162c07c54678e4c049cfb64ac8
고윤혁 <sksgur3217@gmail.com>
2022-07-12 04:35:03 +0900

    WebSokets in NodeJS

--- src/server.js
+++ src/server.js
@@ -1,4 +1,7 @@
+const http = require('http');
+const WebSoket = require('ws');
 const express = require('express');
+
 const app = express();
 
 // pug로 view engine을 설정, express에 template이 어디 있는지 지정
@@ -9,8 +12,13 @@ app.set('views', __dirname + '/views');
 app.use('/public', express.static(__dirname + '/public'));
 
 // home.pug를 render 해주는 router handler를 만든다.
-app.get('/', (req, res) => res.render('home')); // router handler, 현재 프로젝트에서는 home url만 쓸 것이다.
-app.get('/*', (req, res) => res.redirect('/')); // user가 어떤 url로 이동하던지 home으로 돌려보낸다.
+app.get('/', (_, res) => res.render('home')); // router handler, 현재 프로젝트에서는 home url만 쓸 것이다.
+app.get('/*', (_, res) => res.redirect('/')); // user가 어떤 url로 이동하던지 home으로 돌려보낸다.
 
 const handleListen = () => console.log(`Listening in http://localhost:3000/`);
-app.listen(3000, handleListen);
+
+// 같은 서버에서 http, webSoket 둘 다 작동 시킬 수 있다.
+const server = http.createServer(app);
+const wss = new WebSoket.Server({ server });
+
+server.listen(3000, handleListen);
