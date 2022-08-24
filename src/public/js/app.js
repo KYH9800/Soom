5c66f1515de77eb95c32ce8c01a3e69cbc7c75ac
고윤혁 <sksgur3217@gmail.com>
2022-08-25 04:30:53 +0900

    Recap

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,10 +3,14 @@ const socket = io();
 const welcome = document.getElementById('welcome');
 const form = welcome.querySelector('form');
 
+function backendDone(roomName) {
+  console.log(`${roomName} 방이 생성 되었습니다.`);
+}
+
 function handleRoomSubmit(event) {
   event.preventDefault();
   const input = form.querySelector('input');
-  socket.emit('enter_room', { payload: input.value });
+  socket.emit('enter_room', input.value, backendDone);
   input.value = '';
 }
 
--- src/server.js
+++ src/server.js
@@ -20,8 +20,11 @@ const httpServer = http.createServer(app);
 const io = SocketIO(httpServer);
 
 io.on('connection', (socket) => {
-  socket.on('enter_room', (msg) => {
-    console.log(msg);
+  socket.on('enter_room', (roomName, done) => {
+    console.log(roomName);
+    setTimeout(() => {
+      done(roomName);
+    }, 2000);
   });
 });
 
